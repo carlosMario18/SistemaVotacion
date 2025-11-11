818f06be078d0089fca40d1e5dd98bccc8a86f9b
carlosMario18 <2420201003@estudiantesunibague.edu.co>
2025-11-11 18:01:08 -0500

    postgresql as database

--- voting_app/voting_app/settings.py
+++ voting_app/voting_app/settings.py
@@ -17,7 +17,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / '.env')
+# Cargar .env desde la carpeta raíz del proyecto (un nivel arriba de voting_app)
+dotenv_path = BASE_DIR.parent / '.env'
+load_dotenv(dotenv_path)
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,10 +80,16 @@ WSGI_APPLICATION = 'voting_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
+
 }
 
 
