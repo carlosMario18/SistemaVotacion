8cb1a21d25b619f8f7ba5e670f4651c0cfe5feb5
carlosMario18 <2420201003@estudiantesunibague.edu.co>
2025-11-21 17:14:45 -0500

    railway

--- voting_app/voting_app/settings.py
+++ voting_app/voting_app/settings.py
@@ -94,11 +94,11 @@ DATABASES = {
 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("PGDATABASE"),
-        'USER': os.getenv("PGUSER"),
-        'PASSWORD': os.getenv("PGPASSWORD"),
-        'HOST': os.getenv("PGHOST", "127.0.0.1"),
-        'PORT': os.getenv("PGPORT", "5432"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DB_PORT", "5432"),
     }
 
 }
